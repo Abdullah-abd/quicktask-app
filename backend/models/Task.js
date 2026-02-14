c2bbedc3e7978f84e0b2a47ef9fa3e9ee898b0ba
Abdullah <abdullah262001@gmail.com>
2026-02-14 16:26:58 +0530

    created task schema

---
+++ backend/models/Task.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Low",
+    },
+
+    status: {
+      type: String,
+      enum: ["Todo", "In Progress", "Completed"],
+      default: "Todo",
+    },
+
+    dueDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Task", taskSchema);
